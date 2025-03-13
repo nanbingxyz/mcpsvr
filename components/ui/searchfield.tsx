e8292da6e6d061ff8e4f1514dee82136f9d09816
wangweiwei <will_workhard@163.com>
2025-03-13 21:22:45 +0800

    refactor: optimize mobile search experience

--- components/ui/searchfield.tsx
+++ components/ui/searchfield.tsx
@@ -36,7 +36,7 @@ function SearchFieldInput({ className, ...props }: AriaInputProps) {
     <AriaInput
       className={composeRenderProps(className, (className) =>
         cn(
-          "min-w-0 flex-1 bg-background px-2 py-1.5 outline outline-0 placeholder:text-muted-foreground [&::-webkit-search-cancel-button]:hidden",
+          "min-w-0 flex-1 bg-background px-2 py-1.5 outline outline-0 focus:outline-none text-base placeholder:text-muted-foreground [&::-webkit-search-cancel-button]:hidden",
           className
         )
       )}
